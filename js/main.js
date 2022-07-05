0f3a36bdf085747cec4e278d05bb604f060f6bf3
Áureo Rodrigues <94138315+Aurinho007@users.noreply.github.com>
2022-07-05 17:28:06 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -117,8 +117,8 @@ function desenha_boneco(qnt_erros){
 }
 
 //Gera palavra
-let index = (Math.random() * palavras_possiveis.length).toFixed()
-if(index > 15 || index < 0) index = 5
+let index = (Math.random() * palavras_possiveis.length-1).toFixed()
+if(index < 0) index++ //Gambiarra para arrumar um bug
 let palavra_sorteada = palavras_possiveis[index].palavra
 let dica_palavra_sorteada = palavras_possiveis[index].dica
 let letras_usadas = []
